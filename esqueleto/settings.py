cd1bca7da47aceab8db3f5047b1d8e0239dc2ab6
Alan00Aquino <133703934+Alan00Aquino@users.noreply.github.com>
2023-06-30 20:12:01 -0300

    Modificación en html y css, conexión con database

--- esqueleto/settings.py
+++ esqueleto/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'esqueleto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'esqueleto',
+        'USER': 'root',
+        'PASSWORD': 'F3rn3t4340.A',
+        'HOST': 'localhost',  
+        'PORT': '3306',  
     }
 }
 
